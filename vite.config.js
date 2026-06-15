feaf5292a5a8b947f9b333a9c41ba76115d30044
ducanh <ducanh2k5.thachthat@gmail.com>
2026-06-16 01:31:09 +0700

    chore: restore GitHub Pages deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Đổi thành '/' khi dùng custom domain (internjobfinder.me)
-  // Đổi lại '/InternJobFinder/' nếu dùng github.io/InternJobFinder
-  base: '/',
+  base: '/InternJobFinder/',
 })
